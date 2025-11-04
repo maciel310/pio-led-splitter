cd51452b6a1966559c4b1a180063b64d585c5e6e
maciel310 <maciel310@gmail.com>
2025-11-03 22:23:09 -0700

    Add pio for copying bit patterns from one pin to another. Allows manually changing output strip by recompiling, but nothing dynamic.

--- pio-led-splitter.cpp
+++ pio-led-splitter.cpp
@@ -2,36 +2,27 @@
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 
-#include "blink.pio.h"
+#include "ws28xx-splitter.pio.h"
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
+#define SOURCE_PIN 28
+#define OUT_START_PIN 0
+#define OUT_PIN_COUNT 1
+
+void init_splitter_pio(PIO pio, uint sm, uint offset) {
+    ws28xx_splitter_init(pio, sm, offset, SOURCE_PIN, OUT_START_PIN, OUT_PIN_COUNT);
     pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
 }
 
 int main() {
     stdio_init_all();
 
-    // PIO Blinking example
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
+    uint offset = pio_add_program(pio, &ws28xx_splitter_program);
     printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    init_splitter_pio(pio, 0, offset);
 
     while (true) {
-        printf("Hello, world!\n");
         sleep_ms(1000);
     }
 }
