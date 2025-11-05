36a76af0c6b998df40e4a5c4fbe45580c1f3e297
maciel310 <maciel310@gmail.com>
2025-11-04 22:31:18 -0700

    Implement second PIO program to watch for RESET signal from LED input to synchronize state. Also fix two off-by-one errors in the Splitter PIO.

--- pio-led-splitter.cpp
+++ pio-led-splitter.cpp
@@ -8,21 +8,27 @@
 #define OUT_START_PIN 0
 #define OUT_PIN_COUNT 3
 
-void init_splitter_pio(PIO pio, uint sm, uint offset) {
-    ws28xx_splitter_init(pio, sm, offset, SOURCE_PIN, OUT_START_PIN, OUT_PIN_COUNT);
-    pio_sm_set_enabled(pio, sm, true);
+void init_splitter_pio(PIO pio, uint splitter_sm, uint splitter_offset, uint reset_watch_sm, uint reset_watch_offset) {
+    ws28xx_splitter_init(pio, splitter_sm, splitter_offset, SOURCE_PIN, OUT_START_PIN, OUT_PIN_COUNT);
+    ws28xx_reset_watch_init(pio, reset_watch_sm, reset_watch_offset, SOURCE_PIN);
 }
 
 int main() {
     stdio_init_all();
 
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &ws28xx_splitter_program);
-    printf("Loaded program at %d\n", offset);
+    uint splitter_sm = 0;
+    uint reset_watch_sm = 1;
 
-    init_splitter_pio(pio, 0, offset);
+    init_splitter_pio(pio, splitter_sm, pio_add_program(pio, &ws28xx_splitter_program), reset_watch_sm, pio_add_program(pio, &ws28xx_reset_watch_program));
 
     while (true) {
         sleep_ms(1000);
+        // // Number of strands
+        // pio_sm_put_blocking(pio, 0, (1 << OUT_PIN_COUNT));
+        // // Count of pixels per strand
+        // pio_sm_put_blocking(pio, 0, 5);
+        // pio_sm_put_blocking(pio, 0, 5);
+        // pio_sm_put_blocking(pio, 0, 5);
     }
 }
