bf43bf34d6a7c54033463002bcf2851727c53d6f
maciel310 <maciel310@gmail.com>
2025-11-05 19:01:31 -0700

    Update reset logic so microcontroller handles the reset instead of directly via the state machines. This allows smarter refilling of the FIFO. Also add logic to reset state machine on reset, which should reduce the chance of desynchronization.

--- pio-led-splitter.cpp
+++ pio-led-splitter.cpp
@@ -8,7 +8,31 @@
 #define OUT_START_PIN 0
 #define OUT_PIN_COUNT 3
 
+PIO pio = pio0;
+uint splitter_sm = 0;
+uint reset_watch_sm = 1;
+uint splitter_offset;
+uint reset_watch_offset;
+
+void ws28xx_reset_watch_handler() {
+    ws28xx_splitter_reset(pio, splitter_sm, splitter_offset);
+
+    // TODO: Move this to DMA so more than 4 messages can be sent in the IRQ without blocking.
+    // Number of strands
+    pio_sm_put_blocking(pio, splitter_sm, (1 << OUT_PIN_COUNT));
+    // Count of pixels per strand
+    pio_sm_put_blocking(pio, splitter_sm, 6-1);
+    pio_sm_put_blocking(pio, splitter_sm, 10-1);
+    pio_sm_put_blocking(pio, splitter_sm, 21-1);
+
+    pio_interrupt_clear(pio, 0);
+}
+
 void init_splitter_pio(PIO pio, uint splitter_sm, uint splitter_offset, uint reset_watch_sm, uint reset_watch_offset) {
+    pio_set_irq0_source_enabled(pio, pis_interrupt0, true);
+    irq_set_exclusive_handler(pio_get_irq_num(pio, 0), ws28xx_reset_watch_handler);
+    irq_set_enabled(pio_get_irq_num(pio, 0), true);
+
     ws28xx_splitter_init(pio, splitter_sm, splitter_offset, SOURCE_PIN, OUT_START_PIN, OUT_PIN_COUNT);
     ws28xx_reset_watch_init(pio, reset_watch_sm, reset_watch_offset, SOURCE_PIN);
 }
@@ -16,20 +40,10 @@ void init_splitter_pio(PIO pio, uint splitter_sm, uint splitter_offset, uint res
 int main() {
     stdio_init_all();
 
-    PIO pio = pio0;
-    uint splitter_sm = 0;
-    uint reset_watch_sm = 1;
-
-    init_splitter_pio(pio, splitter_sm, pio_add_program(pio, &ws28xx_splitter_program), reset_watch_sm, pio_add_program(pio, &ws28xx_reset_watch_program));
+    splitter_offset = pio_add_program(pio, &ws28xx_splitter_program);
+    reset_watch_offset = pio_add_program(pio, &ws28xx_reset_watch_program);
+    init_splitter_pio(pio, splitter_sm, splitter_offset, reset_watch_sm, reset_watch_offset);
 
     while (true) {
-        // TODO - synchronize this with thte reset_watch PIO?
-
-        // Number of strands
-        pio_sm_put_blocking(pio, splitter_sm, (1 << OUT_PIN_COUNT));
-        // Count of pixels per strand
-        pio_sm_put_blocking(pio, splitter_sm, 6-1);
-        pio_sm_put_blocking(pio, splitter_sm, 10-1);
-        pio_sm_put_blocking(pio, splitter_sm, 21-1);
     }
 }
